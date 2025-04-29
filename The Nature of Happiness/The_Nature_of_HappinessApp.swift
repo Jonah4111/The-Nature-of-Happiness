38c4d398e1d9df835fe8745f41fbd09a224b09dc
jonah4111 <jonah.hirst44@gmail.com>
2025-04-28 20:52:27 -0600

    Initial Commit

---
+++ The Nature of Happiness/The_Nature_of_HappinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Nature_of_HappinessApp.swift
+//  The Nature of Happiness
+//
+//  Created by Jonah Hirst on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct The_Nature_of_HappinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ The Nature of HappinessTests/The_Nature_of_HappinessTests.swift
@@ -0,0 +1,17 @@
+//
+//  The_Nature_of_HappinessTests.swift
+//  The Nature of HappinessTests
+//
+//  Created by Jonah Hirst on 4/28/25.
+//
+
+import Testing
+@testable import The_Nature_of_Happiness
+
+struct The_Nature_of_HappinessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
